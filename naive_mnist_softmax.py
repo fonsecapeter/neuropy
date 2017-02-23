e842f5e636d785204a7a1a249f89a3c97eb408f3
Peter Fonseca <pfonseca@23andme.com>
2017-02-23 08:55:18 -0800

    naive softmax

---
+++ naive_mnist_softmax.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+from sample_data import cannabis
+
+# cannabis is all the data
+# maybe more data is called for :)
+#   cannabis.train: 33 points
+#   cannabis.test: 4 points
+#   cannabis.validation: 5 points
+#
+# mnist.train.images are the mris flattened from
+# 256 * 256 * 170 = 11141120
+
+x = tf.placeholder(tf.float32, [None, 11141120])
+
+W = tf.Variable(tf.zeros([11141120, 2]))
+b = tf.Variable(tf.zeros([2]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# -------------------------------------------------------------------------------
+# train
+y_ = tf.placeholder(tf.float32, [None, 2])
+
+# implement cross entropy function
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+# ask tf to minimize this loss
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+# launch the model in an interactive session
+sess = tf.InteractiveSession()
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+tf.initialize_all_variables().run()
+# train 3 times
+for i in range(3):
+    batch_xs, batch_ys = cannabis.train.next_batch(50)
+    train_accuracy = accuracy.eval(feed_dict={
+        x: batch_xs, y_: batch_ys
+    })
+    print('step %d, training accuracy: %g' % (i, train_accuracy))
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# -------------------------------------------------------------------------------
+# display the accuracy
+print('test accuracy:', sess.run(accuracy, feed_dict={x: cannabis.test.images, y_: cannabis.test.labels}))
+# step 0, training accuracy: 0.58
+# step 1, training accuracy: 0.6
+# step 2, training accuracy: 0.46
+# test accuracy: 0.421053
+# just terrible
--- sample_data.py
+++ sample_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import nibabel as nib
 from tf_data import TFDataSet
+from util.progress import print_progress
 
 # one-hot class label
 classes = {'HC': [1, 0], 'CB': [0, 1]}
@@ -10,13 +11,17 @@ participants = np.genfromtxt('cannabis_sample_data/participants.tsv', dtype=str,
 
 images = []
 labels = []
-for participant in participants:
+total = len(participants) - 1
+for idx, participant in enumerate(participants):
     p_id = participant[0]
     p_class = participant[1]
 
-    # import just  followup b/c all are of the same shape (one bl is off)
-    fu = nib.load('cannabis_sample_data/sub-%s/ses-FU/anat/sub-%s_ses-FU_T1w.nii.gz' % (p_id, p_id))
-    images.append(fu.get_data())
-    labels.append(classes[p_class])
+    for _ in range(5):
+        # multiply data by 5 for kicks, don't try at home!
+        # import just  followup b/c all are of the same shape (one bl is off)
+        fu = nib.load('cannabis_sample_data/sub-%s/ses-FU/anat/sub-%s_ses-FU_T1w.nii.gz' % (p_id, p_id))
+        images.append(fu.get_data().flatten())
+        labels.append(classes[p_class])
+    print_progress(idx, total, prefix='Loading images:', length=40)
 
 cannabis = TFDataSet(np.array(images), np.array(labels))
