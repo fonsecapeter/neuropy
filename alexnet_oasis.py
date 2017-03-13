896eb4ebe1fa4641fe62752527f88a1a270a1096
Peter Fonseca <peter.nfonseca@gmail.com>
2017-03-13 03:43:39 +0000

    Add alexnet 3d clone

--- alexnet_oasis.py
+++ alexnet_oasis.py
@@ -1,3 +1,4 @@
+# https://github.com/WeiTang114/MVCNN-TensorFlow
 import numpy as np
 import math
 import tensorflow as tf
@@ -7,15 +8,27 @@ from util.progress import print_progress
 LEARNING_RATE = 0.000001
 MOMENTUM = 0.3
 
-BATCH_SIZE = 50
-NUM_EPOCHS = 200
+BATCH_SIZE = 60
+NUM_EPOCHS = 800
 
-# PATCH_SIZE = math.ceil(oasis.image_shape[0] * 0.1)
-# STRIDE_SIZE = math.ceil(oasis.image_shape[0] * 0.1)
-PATCH_SIZE = 5 
-DEPTH = 16
+PATCH_1 = 9 
+DEPTHi_1 = 96
 
-H_SIZE = 64
+PATCH_2 = 5
+DEPTH_2 = 256
+
+PATCH_3 = 3
+DEPTH_3 = 384
+
+PATCH_4 = 3
+DEPTH_4 = 384
+
+PATCH_5 = 3
+DEPTH_5 = 256
+
+CONNECTED_SIZE = 49
+CONNECTED_DEPTH = 4096
+
 REG_BETA = 0.0005
 KEEP_PROB = 0.5
 
@@ -25,52 +38,75 @@ shape = oasis.image_shape
 def accuracy(predictions, labels):
     return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
 
-
 graph = tf.Graph()
 with graph.as_default():
     x = tf.placeholder(tf.float32, shape=[None, shape[0], shape[1], shape[2],  1])
     y_ = tf.placeholder(tf.float32, shape=[None, oasis.num_labels])
     # -------------------------------------------------------------------------------
-    # First Convolutional Layer
-    W_1 = tf.Variable(tf.truncated_normal(np.asarray([PATCH_SIZE, PATCH_SIZE, PATCH_SIZE, 1, DEPTH], dtype='int64'), stddev=0.1))
-    b_1 = tf.Variable(tf.zeros([DEPTH]))
-    # -------------------------------------------------------------------------------
-    # Second Convolutional Layer
-    W_2 = tf.Variable(tf.truncated_normal([1, 1, 1, DEPTH, DEPTH], stddev=0.1))
-    b_2 = tf.Variable(tf.constant(1.0, shape=[DEPTH]))
+    # Convolutional Layers
+    W_1 = tf.Variable(tf.truncated_normal(np.asarray([PATCH_1, PATCH_1, PATCH_1, 1, DEPTH_1], dtype='int64'), stddev=0.1))
+    b_1 = tf.Variable(tf.zeros([DEPTH_1]))
+    
+    W_2 = tf.Variable(tf.truncated_normal([PATCH_2, PATCH_2, PATCH_2, DEPTH_1, DEPTH_2], stddev=0.1))
+    b_2 = tf.Variable(tf.constant(1.0, shape=[DEPTH_2]))
+    
+    W_3 = tf.Variable(tf.truncated_normal([PATCH_3, PATCH_3, PATCH_3, DEPTH_2, DEPTH_3], stddev=0.1))
+    b_3 = tf.Variable(tf.constant(1.0, shape=[DEPTH_3]))
+
+    W_4 = tf.Variable(tf.truncated_normal([PATCH_4, PATCH_4, PATCH_4, DEPTH_3, DEPTH_4], stddev=0.1))
+    b_4 = tf.Variable(tf.constant(1.0, shape=[DEPTH_4]))
+
+    W_5 = tf.Variable(tf.truncated_normal([PATCH_5, PATCH_5, PATCH_5, DEPTH_4, DEPTH_5], stddev=0.1))
+    b_5 = tf.Variable(tf.constant(1.0, shape=[DEPTH_5]))
+
+    W_6 = tf.Variable(tf.truncated_normal([CONNECTED_SIZE*CONNECTED_SIZE*DEPTH_5, CONNECTED_DEPTH]], stddev=0.1))
+    b_6 = tf.Variable(tf.constant(1.0, shape=[CONNECTED_DEPTH]))
+
+    W_6 = tf.Variable(tf.truncated_normal([CONNECTED_DEPTH, CONNECTED_DEPTH]], stddev=0.1))
+    b_6 = tf.Variable(tf.constant(1.0, shape=[CONNECTED_DEPTH]))
     # -------------------------------------------------------------------------------
     # Output Layer
-    W_o = tf.Variable(tf.truncated_normal([H_SIZE, oasis.num_labels]))
+    W_o = tf.Variable(tf.truncated_normal([CONNECTED_DEPTH, oasis.num_labels]))
     b_o = tf.Variable(tf.zeros([oasis.num_labels]))
 
     def model(data, train=False):
-        conv = tf.nn.conv3d(x, W_1, [1, 2, 2, 2, 1], padding='VALID')
+        conv = tf.nn.conv3d(x, W_1, [1, 4, 4, 4, 1], padding='SAME')
         hidden = tf.nn.relu(conv + b_1)
         pool = tf.nn.max_pool3d(
             hidden,
-            [1, 2, 2, 2, 1],
-            [1, 2, 1, 2, 1],
-            padding='VALID'
+            ksize=[1, 3, 3, 3, 1],
+            stride=[1, 2, 2, 2, 1],
+            padding='SAME'
         )
 
-        conv = tf.nn.conv3d(pool, W_2, [1, 1, 1, 1, 1], padding='VALID')
-        hidden = tf.nn.relu(conv * b_2)
+        conv = tf.nn.conv3d(pool, W_2, [1, 1, 1, 1, 1], padding='SAME')
+        hidden = tf.nn.relu(conv + b_2)
+
+        conv = tf.nn.conv3d(conv, W_3, [1, 1, 1, 1, 1], padding='SAME')
+
+        conv = tf.nn.conv3d(conv, W_4, [1, 1, 1, 1, 1], padding='SAME') 
+
+        conv = tf.nn.conv3d(conv, W_5, [1, 1, 1, 1, 1], padding='SAME') 
         pool = tf.nn.max_pool3d(
-            hidden,
-            [1, 2, 2, 2, 1],
-            [1, 2, 2, 2, 1],
-            padding='VALID'
+            conv,
+            ksize=[1, 3, 3, 3, 1],
+            stride=[1, 2, 2, 2, 1],
+            padding='SAME'
         )
         # -------------------------------------------------------------------------------
-        # Reshape
-        flat_shape = np.prod(pool.get_shape().as_list()[1:])
-        W_3 = tf.Variable(tf.truncated_normal([flat_shape, H_SIZE], stddev=0.1))
-        b_3 = tf.Variable(tf.constant(1.0, shape=[H_SIZE]))
-        reshape = tf.reshape(pool, [-1, flat_shape])
-        hidden = tf.nn.relu(tf.matmul(reshape, W_3) + b_3)
+        # Reshape & Fully Connected
+        # flat_shape = np.prod(pool.get_shape().as_list()[1:])
+        # W_3 = tf.Variable(tf.truncated_normal([flat_shape, H_SIZE], stddev=0.1))
+        # b_3 = tf.Variable(tf.constant(1.0, shape=[H_SIZE]))
+        # reshape = tf.reshape(pool, [-1, flat_shape])
+        reshape = tf.reshape(pool, [-1, CONNECTED_SIZE*CONNECTED_SIZE*DEPTH_5])
+        fc = tf.nn.tanh(tf.matmul(reshape, W_6) + b_6)
+        fc = tf.dropout(fc, KEEP_PROB)
 
-        hidden = tf.nn.dropout(hidden, KEEP_PROB)
-        return tf.matmul(hidden, W_o) + b_o
+        fc = tf.nn.tanh(tf.matmul(fc, W_7) + b_7)
+        fc = tf.dropout(fc, KEEP_PROB)
+
+        return tf.matmul(fc, W_o) + b_o
     # -------------------------------------------------------------------------------
     # Train
     logits = model(x, train=True)
